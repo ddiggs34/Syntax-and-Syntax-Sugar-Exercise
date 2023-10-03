4e7691090bd35110bb9f19a81c7b8d3509c4d95f
David Diggs <ddiggs34@gmail.com>
2023-10-03 11:40:50 -0400

    exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var answer = 13;
+            //string response;
+
+            // Console.WriteLine((answer <= 9) ? response = answer + " is less than nine" : response = answer + " is greater than or equal to nine" )  ;
+
+            var answer = 15;
+            var response = (answer <= 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+
+
+
+
         }
     }
 }
